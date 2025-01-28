8457e9c02621bf87d67a38005124c51da8e2eb8f
Jonathan200611613 <andressguilera2006@gmail.com>
2025-01-27 20:50:42 -0500

    cambios asegurados

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 
 // 1. IMPORTAR LAS DEPENDENCIAS Y MÓDULOS QUE NECESITAMOS
-import express from 'express'; 
-import dotenv from 'dotenv'; 
+import express from 'express';
+import dotenv from 'dotenv';
 import { connectionMongo } from './src/config/dataBase.js';
 import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
-import cors from 'cors'; 
+import cors from 'cors';
 
 // 2. configurar el uso de nuestro servidor y dependencias
-const app = express(); 
-dotenv.config(); 
+const app = express();
+dotenv.config();
 connectionMongo();
-app.use(cors()); 
+app.use(cors());
 
 // Le indico las rutas que debe utilizar
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
+
+app.get("/", (req, res) => {
+    res.send("Servidor ejecutándose correctamente");
+});
 
 export default app;
